81e6e90110e6c337c653cbfe99fc30612dfa225b
kmtompkins3 <kmtompkins3@gmail.com>
2016-03-20 19:18:28 -0400

    Shape Detected

--- Live Edges/Live Edges/Source.cpp
+++ Live Edges/Live Edges/Source.cpp
@@ -16,11 +16,13 @@ vector<Vec4i> hierarchy;
 RNG rng(12345);
 Scalar RED(0, 0, 255);//BGR
 Scalar GREEN(0, 255, 0);
+Scalar WHITE(255, 255, 255);
 
 
 VideoCapture cap(0); // open the default camera
 
 long double Distance( long double point1x, long double point1y, long double point2x, long double point2y);
+bool PointCompare(Point2f point1, Point2f point2);
 
 
 
@@ -55,7 +57,7 @@ int main()
 		for (int i = 0; i < contours.size(); i++)//draws all contours
 		{
 			///drawContours(mat drawing, vector vector point, what contours to draw, color or contors,line thickness, line type, hierarchy, maxlevel ,offset
-			if (contours[i].size() >= 1)
+			if (contours[i].size() >= 30)
 			{
 				Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));//random color for each contour
 				drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());//draws the contour image
@@ -69,6 +71,7 @@ int main()
 			vector<vector<Point> > contours_poly(contours.size());
 			vector<Rect> boundRect(contours.size());
 			vector<Point2f>center(contours.size());
+			vector<Point2f>CenterPoints(contours.size());
 			vector<float>radius(contours.size());
 			for (int i = 0; i < contours.size(); i++)
 			{
@@ -78,16 +81,32 @@ int main()
 			}
 			for (int i = 0; i < contours.size(); i++)
 			{
-				if(boundRect[i].area() > 5000)
+				if (boundRect[i].area() > 1000)
+				{
+					CenterPoints[i] = Point2f((boundRect[i].width / 2) + boundRect[i].x, (boundRect[i].height / 2) + boundRect[i].y);
+				}
+			}
+			for (int i = 0; i < contours.size(); i++)
+			{
+				if (boundRect[i].area() > 1000)
+				{
 					rectangle(frame, boundRect[i].tl(), boundRect[i].br(), RED, 2, 8, 0);
+					circle(frame, Point2f((boundRect[i].width/2)+ boundRect[i].x,(boundRect[i].height/2)+ boundRect[i].y), 5, WHITE, 1, 8, 0);
+				}
+
+			}
+			for (int i = 0; i < contours.size(); i++)
+			{
+				for (int x = 0; x < contours.size(); x++)
+				{
+					if (PointCompare(CenterPoints[i], CenterPoints[x]) && i != x)
+					{
+						circle(frame, (CenterPoints[i], CenterPoints[x]), 5, GREEN, 1, 8, 0);
+					}
+				}
 			}
 
 			///Rotated bounding box
-			long double side1;
-			long double side2;
-			long double side3;
-			long double side4;
-			long double area;
 			vector<RotatedRect> rotatedRect(contours.size());
 			for (int i = 0; i < contours.size(); i++)
 			{
@@ -97,11 +116,10 @@ int main()
 			{
 				Point2f rect_points[4]; rotatedRect[i].points(rect_points);
 
-				for (int j = 0; j < 4; j++)
+				//for (int j = 0; j < 4; j++)
 					//if((Distance(rect_points[j].x, rect_points[j].y, rect_points[(j + 1) % 4].x, rect_points[(j + 1) % 4].y)) > .5)
-						line(frame, rect_points[j], rect_points[(j + 1) % 4], GREEN, 1, 8);
+						//line(frame, rect_points[j], rect_points[(j + 1) % 4], GREEN, 1, 8);
 			}
-			
 			imshow("Bounding retangle", frame);
 		}
 		catch (...) {}
@@ -121,3 +139,15 @@ long double Distance( long double point1x, long double point1y, long double poin
 
 	return(distance);
 }
+bool PointCompare(Point2f point1, Point2f point2)
+{
+	if(abs(point1.x-point2.x) < 5 && abs(point1.y-point2.y) < 5)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+//640x480
